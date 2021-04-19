ffe13705e8028b23babdff639869fe223fd034d8
Bruno Garcia Lima <brunogarciawork@gmail.com>
2021-04-19 20:47:26 -0300

    comit 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
