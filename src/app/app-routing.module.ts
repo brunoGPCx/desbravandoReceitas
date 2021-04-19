e5de27d8acc1c910362b23bc409eb5da3185475f
Bruno Garcia Lima <brunogarciawork@gmail.com>
2021-04-19 16:18:28 -0300

    comit 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,19 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'termos',
+    loadChildren: () => import('./termos/termos.module').then( m => m.TermosPageModule)
+  },
+  {
+    path: 'suporte',
+    loadChildren: () => import('./suporte/suporte.module').then( m => m.SuportePageModule)
   }
+
 ];
 @NgModule({
   imports: [
